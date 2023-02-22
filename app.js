7f9b1b9d4476061e1fd87e98e9a7fd5eb21eddde
kerBamf <zach.pedigo@gmail.com>
2023-02-22 00:18:03 -0500

    Refined combat logic to shift enemy ships after one is destroyed

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
+let playerShipName = 'USS Assembly'
+
 //Build Player Ship
 let playerShip = {
-    name: 'USS Assembly',
+    name: playerShipName,
     hull: 20,
     firePower: 5,
     accuracy: .7,
     attack(enemy) {
-        let attackScore = Math.random();
-        if (attackScore < this.accuracy) {
+        let attackTest = Math.random();
+        console.log("Firing weapons!")
+        if (attackTest < this.accuracy) {
             enemy.hull -= 5;
             if (enemy.hull > 0) {
             console.log(`We've scored a hit on ${enemy.name}, captain!`)
-            } else if (enemy.hull = 0) {
+            } else if (enemy.hull <= 0 && enemy.hull > -2) {
                 console.log(`Confirmed kill, captain! ${enemy.name} destroyed!`)
-            } else if (enemy.hull < 0 ) {
+            } else if (enemy.hull <= -2) {
                 console.log(`That's a critical hit, Captain! ${enemy.name} has been obliterated!`)
             }
         } else {
@@ -39,10 +42,17 @@ class Ship {
         this.accuracy = getRandomInt(6, 8) * .1
     }
         attack(enemy) {
-            let attackScore = Math.random();
-            if (attackScore < this.accuracy) {
+            let attackTest = Math.random();
+            if (attackTest < this.accuracy) {
                 enemy.hull -= this.firepower
-                console.log(`${this.name} landed a hit on the ${enemy.name}! She has ${enemy.hull} hull points left!`)
+                if (enemy.hull <= 0) {
+                    console.log("She's taken too much damage, Captain! We need to abandon ship!")
+                } else if (enemy.hull > 0) {
+                    console.log(`${this.name} landed a hit on the ${enemy.name}! She has ${enemy.hull} hull points left!`)
+                }
+                
+            } else {
+                console.log(`We've evaded fire from ${this.name} captain!`)
             }
         }
     };
@@ -57,9 +67,28 @@ function createAlienFleet(num) {
 
 
 createAlienFleet(6);
-console.log(alienFleet)
+//console.log(alienFleet[0])
+
+let currentEnemy = alienFleet[0];
+//console.log(alienFleet.indexOf(currentEnemy))
 
 //Combat Logic
 function commenceAssault() {
-
+    let turn = 0
+    while (playerShip.hull > 0 && currentEnemy.hull > 0) {
+        if (turn == 0) {
+            playerShip.attack(currentEnemy);
+            //console.log(currentEnemy.hull);
+            if (currentEnemy.hull <= 0 && alienFleet.indexOf(currentEnemy) < 5) {
+                currentEnemy = alienFleet[(alienFleet.indexOf(currentEnemy) + 1)]
+            }
+            turn = 1
+        } else {
+            currentEnemy.attack(playerShip);
+            console.log(playerShip.hull);
+            turn = 0
+        }
+    }
 }
+
+commenceAssault();
